74015c7e64db92a36996217aa6faae82c3098fc0
Akshay <akshay.n@plivo.com>
2020-11-12 13:37:09 +0530

    Update upload_file_to_s3.py

--- scripts/pipeline/upload_file_to_s3.py
+++ scripts/pipeline/upload_file_to_s3.py
@@ -14,13 +14,13 @@ def main():
     aws_access_secret=sys.argv[4]
     local_path=sys.argv[5]
 
-    session = boto3.Session(
+    session = boto3.session.Session(
         aws_access_key_id=aws_access_key,
         aws_secret_access_key=aws_access_secret,
     )
-    client = session.client('s3')
+    client = session.resource('s3')
 
-    response = client.upload_file(
+    response = client.meta.client.upload_file(
         Filename=local_path,
         Bucket=bucket_name,
         Key=aws_key
