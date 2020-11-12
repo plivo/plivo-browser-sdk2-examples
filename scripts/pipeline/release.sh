97d6017c4eefbce1cfaf997d4f876bf29fbf841e
Akshay <akshay.n@plivo.com>
2020-11-12 14:42:50 +0530

    Update release.sh

--- scripts/pipeline/release.sh
+++ scripts/pipeline/release.sh
@@ -4,11 +4,8 @@ aws_access_key=$3
 aws_access_secret=$4
 local_path=$5
 
-# Remove any existing versions of files
-rm -rf $local_path
-
-# Create a zip of the current directory.
-zip -r $local_path . -x .git/ .git/*** .github/workflows/release.yml scripts/pipeline/release.sh scripts/pipeline/upload_file_to_s3.py .DS_Store
+# echo/print the current directory.
+echo "$local_path"
 
 # Install required dependencies for Python script.
 pip3 install boto3
