93367a2950a85b8f96efdf7404f67dd84695c90e
Mohammed Huzaif Plivo <huzaif@plivo.com>
2024-07-20 22:52:23 +0530

    Update customclient.js

--- webApp-plivo/public/js/customclient.js
+++ webApp-plivo/public/js/customclient.js
@@ -8,7 +8,7 @@ var ringtoneSourceNode;
 let incomingNotificationAlert = null;
 
 var defaultSettings = {
-	"debug":"INFO",
+	"debug":"ALL",
 	"permOnClick":false,
 	"codecs":[  "OPUS", "PCMU" ],
 	"audioConstraints": {
