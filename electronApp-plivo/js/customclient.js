8f22d6f9aadd3131c31b2db8d9b8ade014e132da
Kasi Prasad Plivo <111332689+kasi-plivo@users.noreply.github.com>
2024-09-17 09:20:50 +0530

    Update customclient.js

--- electronApp-plivo/js/customclient.js
+++ electronApp-plivo/js/customclient.js
@@ -105,6 +105,10 @@ function onCallRemoteRinging(){
 	$('#callstatus').html('Ringing...');
 	console.info('onCallRemoteRinging');
 }
+function onCallConnected(){
+        $('#callstatus').html('CallConnected...');
+        console.info('onCallConnected');
+}
 function onCallAnswered(){
 	console.info('onCallAnswered');
 	$('#callstatus').html('Answered');
@@ -582,6 +586,7 @@ function initPhone(username, password){
 	plivoBrowserSdk.client.on('onLogout', onLogout);
 	plivoBrowserSdk.client.on('onLoginFailed', onLoginFailed);
 	plivoBrowserSdk.client.on('onCallRemoteRinging', onCallRemoteRinging);
+	plivoBrowserSdk.client.on('onCallConnected', onCallConnected);
 	plivoBrowserSdk.client.on('onIncomingCallCanceled', onIncomingCallCanceled);
 	plivoBrowserSdk.client.on('onCallFailed', onCallFailed);
 	plivoBrowserSdk.client.on('onCallAnswered', onCallAnswered);
